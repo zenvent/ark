15011db3f95c8cfa1d4a0fb0dc0e65bc70ec8a5c
Zac <zac@zenvent.com>
2023-11-05 15:59:19 -0500

    revert

--- js.js
+++ js.js
@@ -90,33 +90,3 @@ function createCard(creature) {
 
     cardContainer.appendChild(card);
 }
-
-
-
-var input = document.getElementById('search');
-
-function appendCharacter (c) {
-    switch (c) {
-        case 8: // Backspace
-            input.value = input.value.slice(0, -1);
-            break;
-        default:
-            input.value = input.value + String.fromCharCode(c);
-    }
-}
-
-// Keypress gets the keyCode of the current character not key.
-// e.g. pressing the 'A' key will result in 'a' unless 'Shift' is also held.
-window.addEventListener('keypress', function (e) {
-    appendCharacter(e.keyCode);
-});
-
-// Use Keydown to get special keys like Backspace, Enter, Esc.
-window.addEventListener('keydown', function (e) {
-    switch (e.keyCode) {
-        case 8: // Backspace
-            e.preventDefault(); // Stops the backspace key from acting like the back button.
-            appendCharacter(e.keyCode);
-            break;
-    }
-});
